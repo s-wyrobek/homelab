11b2ddc5adab81774b03e3e4fb98dd482476b088
s-wyrobek <wyrobekszymon@gmail.com>
2026-05-28 21:33:16 +0200

    feat(terraform): import existing dns LXC (101) as IaC
    - Add bpg/proxmox provider with API token auth
    - Import dns container (101/AdGuard) into Terraform state
    - credentials.auto.tfvars excluded via .gitignore

---
+++ infrastructure/terraform/proxmox/main.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "~> 0.78"
+    }
+  }
+}
+
+variable "proxmox_api_url" {}
+variable "proxmox_token_id" {}
+variable "proxmox_token_secret" {}
+
+provider "proxmox" {
+  endpoint  = var.proxmox_api_url
+  api_token = "${var.proxmox_token_id}=${var.proxmox_token_secret}"
+  insecure  = true
+}
