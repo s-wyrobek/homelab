468cfc62ffce9a9e0ad9f196eadc0e4dc38692ed
s-wyrobek <wyrobekszymon@gmail.com>
2026-05-29 21:41:19 +0200

    feat(terraform): import all Proxmox resources as IaC
    - LXC 101 dns (AdGuard Home)
    - LXC 102 nextcloud
    - LXC 103 nginx reverse proxy
    - LXC 104 WireGuard VPN
    - VM 100 debian-01 (k3s node)

---
+++ infrastructure/terraform/proxmox/lxc-VPNwireguard.tf
@@ -0,0 +1,46 @@
+resource "proxmox_virtual_environment_container" "VPNwireguard" {
+  node_name    = "Proxmox"
+  vm_id        = 104
+  unprivileged = true
+
+  initialization {
+    hostname = "VPNwireguard"
+    ip_config {
+      ipv4 {
+        address = "192.168.1.140/24"
+        gateway = "192.168.1.1"
+      }
+    }
+  }
+
+  cpu {
+    cores = 1
+  }
+
+  memory {
+    dedicated = 256
+    swap      = 256
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    size         = 2
+  }
+
+  network_interface {
+    name     = "eth0"
+    bridge   = "vmbr0"
+    firewall = true
+  }
+
+  operating_system {
+    template_file_id = "local:vztmpl/debian-12-standard_12.12-1_amd64.tar.zst"
+    type             = "debian"
+  }
+
+  lifecycle {
+    ignore_changes = [operating_system, initialization[0].dns, console, features]
+  }
+}
+
+
